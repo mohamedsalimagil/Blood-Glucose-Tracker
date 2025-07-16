69c74492ec21f47d1cb5fc6e8ac8d61012542527
Mohamed Salim Agil <mohamedsalimagil.dev@gmail.com>
2025-07-16 22:22:36 +0300

    Blood-Glucose-Tracker

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // get reference to the form element
+    const form = document.getElementById('record-form');
+    const submitBtn = form.querySelector('button');
+const tableBody = document.getElementById('record-table-body');
