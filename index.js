edb6e5fb71fa5925c4c48d2973993b3b9d7d4929
Mohamed Salim Agil <mohamedsalimagil.dev@gmail.com>
2025-07-16 22:23:10 +0300

    Blood-Glucose-Tracker

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById('record-form');
     const submitBtn = form.querySelector('button');
 const tableBody = document.getElementById('record-table-body');
+let currentRecordId= null;
+    function fetchRecords(){
+        fetch("http://localhost:3000/records")//Get request to the Api
+        .then(res => res.json()) //convert the response to json
+        .then(records => {
+            tableBody.innerHTML = ''; // CLEAR any exxisting rows
+             records.forEach(renderRecord); // render each record
+             });
+
+    }
+    
